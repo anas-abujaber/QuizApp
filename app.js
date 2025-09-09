230d3356d33e9be316cd7b1d98498dc86c3f0308
anasadnan2 <anasadnanabojaber@gmail.com>
2025-09-09 20:23:48 +0300

    render result in page (not alert)

--- app.js
+++ app.js
@@ -4,20 +4,19 @@ import { QuizController } from "./quizController.js";
 const quiz = document.getElementById("quiz");
 const submitBtn = document.getElementById("submit");
 const restartBtn = document.getElementById("restart");
+const resultContainer = document.getElementById("result");
 
 const quizApp = new QuizController(questionsData);
 
 quizApp.loadQuestions(quiz);
 
-submitBtn.addEventListener("click", () => {
-  const { correct, total, percent } = quizApp.grade();
-  alert(
-    `Score: ${correct}/${total} (${percent}%)\n${
-      percent >= 70 ? "PASS ✅" : "FAIL ❌"
-    }`
-  );
+restartBtn.addEventListener("click", () => {
+  quizApp.reset(quiz);
 });
 
-restartBtn.addEventListener("click", () => {
-quizApp.reset(quiz);
+submitBtn.addEventListener("click", () => {
+  const { correct, total, percent } = quizApp.grade();
+  resultContainer.textContent = `Score: ${correct}/${total} (${percent}%) → ${
+    percent >= 70 ? "PASS ✅" : "FAIL ❌"
+  }`;
 });
